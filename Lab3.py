c59c41062eacda9fcd7b2059cf8fd449345ca06a
Jimraan <jibril.ali2k@gmail.com>
2020-05-01 05:54:09 -0500

    1 May 2020 5:54am
    Added comments for (1) and (2)
    Verified that (3) actually works and returns the right value
    
    Now to work on (4)

--- Lab3.py
+++ Lab3.py
@@ -3,54 +3,58 @@
 
 # 1. Compute gcd
 def gcd(x, y):
-    midd = y % x
+    midd = y % x    # intermediate midd = y mod x
 
-    if x > y:   # flip the numbers if needed
+    if x > y:       # flip the numbers if needed
         return gcd(y, x)
 
-    if midd == 0: # found GCD
+    if midd == 0:   # found GCD
         return x
-    else:
-        return gcd(midd, x) # recursive call to gcd
+    else:           # recursive call to gcd
+        return gcd(midd, x)
 
     return midd # return GCD
 
 # 2. Compute modular inverse
 def mod_inv(x, y):
-    # r_not = x; ri = y
-    # snot = 1; si = 0
-    # t_not = 0; ti = 1
-    i = 1
+    r_not = x; ri = y
+    snot = 1; si = 0
+    t_not = 0; ti = 1
+    r = 1
 
-    while ((i*x) % y) != 1 and i < y - 1:
-        i += 1
-        # q = r_not // ri # quotient
-        #
-        # r = r_not; r_not = ri
-        # ri = r // ri # remainder
-        #
-        # s = snot; snot = si
-        # si = s - q*si
-        #
-        # t = t_not; tnot = ti
-        # ti = t - q*ti
+    while r != 0:
+        q = r_not // ri     # compute new quotient (int division)
 
-    return i
+        r = r_not % ri      # compute new remainder, assign to placeholder r
+        r_not = ri; ri = r  # swap old remainder with current, and current with new
+
+        s = snot - q*si     # compute new coefficient s
+        snot = si; si = s   # swap old co-eff with current, and current with new
+
+        t = t_not - q*ti    # compute new coefficient t
+        t_not = ti; ti = t  # swap old co-eff with current, and current with new
+
+    return snot % y
 
 # 3. Compute modular exponentiation
 def mod_exp(x, y, n):
-    p = 1; s = x; r = y;
+    p = 1   # p tracks the partial result
+    s = x   # s holds the current value of x^(2^j)
+    r = y   # r is used to help compute the binary expansion of y
 
     while r > 0:
-        if r % 2 == 1:
-            p = p * s % n
-        s = s * s % n
-        r = r // 2
+        if r % 2 == 1:      # if r/2 has a remainder, i.e. if bit j = 1
+            p = p * s % n   # if the bit is 1, multiply p by x^2, then mod n
+        s = s * s % n       # square s, then mod n
+        r = r // 2          # get ready to compute the next bit of r in base 2
 
     return p
+
+# 4. 
 
 
 if __name__ == '__main__':
     test = gcd(462, 1071)
     print(test)
     print(mod_inv(52, 77))
+    print(mod_exp(7, 64, 2399))
